6d0876df2566ea8afc39e1ed9a3cc6d9a1aabad6
Sushant Sharma-Chaudhary <sushant.sharma-chaudhary@cr71.cds.ligo-la.caltech.edu>
2024-05-14 13:38:30 -0500

    doc string for data.py

--- sammie/sammie/data.py
+++ sammie/sammie/data.py
@@ -11,12 +11,46 @@ from scipy.optimize import curve_fit
 import nds2
 
 def noise_model(f, a, na):
-    """Noise model for STS noise"""
+    """Noise model for STS noise
+    
+    Parameters
+    ----------
+    f : array
+        Frequency array
+    a : float
+        frequency power in Noise model
+    na : float
+        Constant term in the Noise model
+
+    Returns
+    -------
+    noise : array
+        Array of noise data 
+    """
 
     noise = ((na/f**a)**2)**.5
     return noise
 
 def fetch_timeseries_data(channel, gps_start, gps_end, mode):
+    """Fetch channel data from either gwpy/cdsutils given 
+    a start and end gps time.
+
+    Parameters
+    ----------
+    channel : str
+        Channel data to be fetched
+    gps_start : int
+        Data start time
+    gps_end : int
+        Data end time
+    mode : str
+        Mode of fetching. Choices gwpy/cdsutils
+    
+    Returns
+    -------
+    data : array
+        Channel data for the given time
+    """
     if mode == 'gwpy':
         data = TimeSeries.fetch(channel, gps_start, gps_end)
     elif mode == 'cdsutils':
@@ -26,12 +60,28 @@ def fetch_timeseries_data(channel, gps_start, gps_end, mode):
 
 
 def coherence_calculator(data_A, data_B):
+    """ Computes coherence between two asd.
+
+    Paraneters:
+    -----------
+    data_A : gwpy.FrequencySeries
+        ASD for channel A
+    data_B : gwpy.FrequencySeries
+        ASD for channel B
+    
+    Returns:
+    --------
+    f : array
+        Array of frequency
+    coh : array
+        Array of coherence value
+    """
     rate = data_A.sample_rate.value
     f,coh = coherence(data_A.value,data_B.value, fs=rate, nperseg=rate*512, noverlap=rate*512/2)
     return f,coh
 
 
-def plot_asd_coherence(data_A, data_B, start_times, cutoff_freq=None):
+def plot_asd_coherence(data_A, data_B, cutoff_freq=None):
 
     f,coh = coherence_calculator(data_A, data_B)
     fig, (ax1,ax2) = plt.subplots(2,1, sharex=True, figsize=(15,6))
@@ -67,8 +117,24 @@ def plot_padded_asd(asd_a, asd_b, cutoff_freq):
     ax.axvline(cutoff_freq, c='r')
     plt.savefig(f"../temp_analysis/padded_asd_{str(int(asd_a.epoch.value))}.png", dpi=300)
 
-def frequency_minima_scipy_groundmotion(data_A, data_B, asd_dis_a, asd_dis_b):
+def frequency_minima_scipy_groundmotion(data_A, data_B, asd_dis_a):
+    """ Finds the first point where coherence btween data
+    drops to a minimal value
 
+    Paraneters:
+    -----------
+    data_A : gwpy.FrequencySeries
+        ASD array for channel A
+    data_B : gwpy.FrequencySeries
+        ASD array for channel B
+    asd_dis_a : gwpy.FrequencySeries
+        ASD array for channel A in displacement units
+    
+    Returns:
+    --------
+    frequency : float
+        Frequency value where ther coherence drops
+    """
     f,coh = coherence_calculator(data_A, data_B)
     # find the point where there is max coherence
     rounded_coh = np.round(coh, 2)
@@ -84,6 +150,24 @@ def frequency_minima_scipy_groundmotion(data_A, data_B, asd_dis_a, asd_dis_b):
 
 
 def pad_asd(asd_a, asd_b, cutoff_freq):
+    """ Pad asd given a cutoff frequency
+
+    Paraneters:
+    -----------
+    asd_a : gwpy.FrequencySeries
+        ASD array for channel A
+    asd_b : gwpy.FrequencySeries
+        ASD array for channel B
+    cutoff_freq : float
+        Frequency value to start cutting off from
+    
+    Returns:
+    --------
+    asd_a : gwpy.FrequencySeries
+        Padded asd for channel A
+    asd_b : gwpy.FrequencySeries
+        Padded asd for channel B
+    """
     start_freq = asd_a.f0
     loc_cutoff = np.where((asd_a.frequencies.value == cutoff_freq))[0][0]
     pad_value_a = asd_a.value[loc_cutoff]
@@ -100,6 +184,26 @@ def pad_asd(asd_a, asd_b, cutoff_freq):
     return asd_a, asd_b
 
 def padded_ground_motion(gpstime, dof):
+    """ Pad asd given a cutoff frequency
+
+    Parameters:
+    -----------
+    gpstime : int
+        start gpstime
+    dof : str
+        Degree of freedom
+    
+    Returns:
+    --------
+    padded_frequencies : array
+        Array of frequency for padded asd
+    padded_asd_value : array
+        Padded asd
+    non_padded_asd_value : array
+        original non-padded asd
+    n_sei : array
+        Adapted seismic noisr array
+    """
     config = configparser.ConfigParser()
     config.read("../etc/config.ini")
     #conn = nds2.connection('nds.ligo-la.caltech.edu',31200)
@@ -133,15 +237,31 @@ def padded_ground_motion(gpstime, dof):
                                 epoch=asd_b_nonzero.epoch, 
                                 channel=asd_b_nonzero.channel)
     displacement_asd = asd_dis_a.copy()
-    cutoff_freq= frequency_minima_scipy_groundmotion(ITMY_data, ITMX_data, asd_dis_a, asd_dis_b)
+    cutoff_freq= frequency_minima_scipy_groundmotion(ITMY_data, ITMX_data, asd_dis_a)
     _,n_sei = conditional_n_sei(cutoff_freq, noise_model, asd_dis_a)
     padded_asd_a, padded_asd_b = pad_asd(asd_dis_a, asd_dis_b, cutoff_freq)
-
-    print(padded_asd_a)
 
     return padded_asd_a.frequencies.value, padded_asd_a.value, displacement_asd.value, n_sei
 
 def conditional_n_sei(cutoff, noise_model, disp_asd):
+    """ Adpated seismic noise
+
+    Parameters:
+    -----------
+    cutoff : float
+        frequency cutoff value
+    noise_model : function
+        Function that serves as noise model
+    disp_asd : gwpy.FrequencySeries
+        ASD array in displacement units
+    
+    Returns:
+    --------
+    frequencies : array
+        Array of frequency for seismic noise
+    n_sei : array
+        Adapted seismic noise array
+    """
     upper = np.max(np.where(disp_asd.frequencies.value == cutoff))
     lower = np.max(np.where((disp_asd.frequencies.value - 1e-2) <0.001))
     x = disp_asd.frequencies.value[lower:upper]
