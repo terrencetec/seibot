99516f65c1d3fda38e386713fa55520ae6395ee3
terrencetec <terrencetec@gmail.com>
2024-05-15 13:30:28 -0500

    pyproject

---
+++ seibot/seibot.py
@@ -0,0 +1,24 @@
+"""SeiBot integration
+"""
+import configparser
+
+#import data
+#import forecast
+#import foton
+#import model
+
+
+#class SeiBot:
+#    """
+#    """
+#    def __init__(self, config):
+#        """
+#        Parameters
+#        ----------
+#        config : str
+#            Path of the configuration file.
+#        """
+#        # Data should give processed ground motion
+#        # 
+#        data = data.Data(config)
+        
