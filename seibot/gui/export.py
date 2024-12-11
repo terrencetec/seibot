0730b4b448148fae5a37f4c4ee4665f3b6116559
terrencetec <terrencetec@gmail.com>
2024-12-11 12:47:42 -0600

    Added GUI export

--- seibot/gui/export.py
+++ seibot/gui/export.py
@@ -32,24 +32,24 @@ class Export(tkinter.LabelFrame):
         self.dropdown = tkinter.OptionMenu(
             self, self.option, *option_list, command=self.update_filter)
 
-        file_label = tkinter.Label(self, text="Foton file: ")
-        self.file = tkinter.Label(self, text="")
-        module_label = tkinter.Label(self, text="Module: ")
-        self.module = tkinter.Label(self, text="")
-        fm_label = tkinter.Label(self, text="FM: ")
-        self.fm = tkinter.Label(self, text="")
+        # file_label = tkinter.Label(self, text="Foton file: ")
+        # self.file = tkinter.Label(self, text="")
+        # module_label = tkinter.Label(self, text="Module: ")
+        # self.module = tkinter.Label(self, text="")
+        # fm_label = tkinter.Label(self, text="FM: ")
+        # self.fm = tkinter.Label(self, text="")
         
         export_button = tkinter.Button(
             self, text="Export", command=self.export)
 
         select_label.grid(row=0, column=0, sticky="w")
         self.dropdown.grid(row=0, column=1, sticky="e")
-        file_label.grid(row=1, column=0, sticky="w")
-        self.file.grid(row=1, column=1, sticky="e")
-        module_label.grid(row=2, column=0, sticky="w")
-        self.module.grid(row=2, column=1, sticky="e")
-        fm_label.grid(row=3, column=0, sticky="w")
-        self.fm.grid(row=3, column=1, sticky="e")
+        # file_label.grid(row=1, column=0, sticky="w")
+        # self.file.grid(row=1, column=1, sticky="e")
+        # module_label.grid(row=2, column=0, sticky="w")
+        # self.module.grid(row=2, column=1, sticky="e")
+        # fm_label.grid(row=3, column=0, sticky="w")
+        # self.fm.grid(row=3, column=1, sticky="e")
         export_button.grid(row=4, column=1, sticky="es")
 
         self.buttons = [self.dropdown, export_button]
@@ -83,16 +83,18 @@ class Export(tkinter.LabelFrame):
     def update_filter(self, _=None):
         """Update filters"""
         filters = self.get_filter()
-        file = filters.sc.filter_file
-        module = filters.sc.module
-        fm = filters.sc.fm
-        self.file.config(text=file)
-        self.module.config(text=module)
-        self.fm.config(text=str(fm))
+        # file = filters.sc.filter_file
+        # module = filters.sc.module
+        # fm = filters.sc.fm
+        # self.file.config(text=file)
+        # self.module.config(text=module)
+        # self.fm.config(text=str(fm))
 
     def export(self):
         """export"""
-        pass
-
-
-
+        path = tkinter.filedialog.asksaveasfilename(
+            title="Save filter configuration file",
+            filetypes=((".ini files", ".ini*"), ("All files", "*.*"))
+        )
+        filters = self.get_filter()
+        filters.export(path)
